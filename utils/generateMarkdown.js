cd2153bcdbc1147389b7ec1ac01a3551cfabcaf7
adeha047 <dehaven7777@gmail.com>
2021-02-18 14:44:25 -0500

    added new features to licenses

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) { 
+  const license = license;
+switch (license) {
+  case 'MIT':
+    return `![License](https://img.shields.io/badge/license-MIT-blue)\n`;
+  
+    case 'GPL':
+    return `![License](https://img.shields.io/badge/license-GPL-blue)\n`;
+    
+    case 'APACHE':
+    return `![License](https://img.shields.io/badge/license-APACHE-blue)\n`;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
